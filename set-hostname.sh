0a975403f55991444151afe1fd019b2f6c50aca6
jmcnatt <jmcnatt@live.com>
2017-01-31 20:51:29 -0500

    Added comments to set-hostname.sh

--- set-hostname.sh
+++ set-hostname.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Sets a Mac's hostname based on the PTR record in DNS
+
 # Get the list of network interfaces
 network_interfaces=(`ifconfig -l`)
 
+# Interate through the network interfaces
+# If the interface has an IP, run host on it
 for i in "${network_interfaces[@]}"
 do
 	if [[ $i == en* ]]
@@ -12,6 +16,8 @@ do
 		if [[ ! -z "$interface_ip" && "$interface_ip" != "" ]]
 		then
 			hostname=`host $interface_ip | awk '{print $5}' | awk -F. '{print $1}'`
+			
+			# If we have an hostname, set it
 			if [[ $hostname != *NXDOMAIN* ]]
 			then
 				scutil --set HostName $hostname
